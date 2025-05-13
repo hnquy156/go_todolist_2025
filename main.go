7e064a6772750bfebf9aaa0e6dfb150e76ed1eaa
hnquy156 <hnquy.hbt@gmail.com>
2025-05-13 20:29:08 +0700

    Init restAPI

--- main.go
+++ main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"time"
 
+	"encoding/json"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/sqlserver"
+	"gorm.io/gorm"
 )
 
 // Tag json, khi giao tiep vs API se gtiep client thong qua ngon ngu trung gian chinh la javascript object notation
-type TodoItems struct {
+type TodoItem struct {
 	Id          int        `json:"id"`
 	Title       string     `json:"title"`
 	Description string     `json:"description"`
@@ -18,25 +26,123 @@ type TodoItems struct {
 	UpdatedAt   *time.Time `json:"updated_at,omitempty"`
 }
 
+type TodoItemCreation struct {
+	Id          int    `json:"-" gorm:"column:id"`
+	Title       string `json:"title" gorm:"column:title"`
+	Description string `json:"description" gorm:"column:description"`
+	// Status      string `json:"status" gorm:"column:status"`
+}
+
+func (TodoItemCreation) TableName() string {
+	return "todo_items"
+}
+
+func (TodoItem) TableName() string {
+	return "todo_items"
+}
+
 func main() {
-	fmt.Println("HELLO")
-
-	now := time.Now().UTC()
-
-	item := TodoItems{
-		Id:          1,
-		Title:       "this is item 1",
-		Description: "this is item 1",
-		Status:      "Doing",
-		CreatedAt:   &now,
-		UpdatedAt:   nil,
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
 	}
 
+	dsn := os.Getenv("DB_CONN_STR")
+	db, err := gorm.Open(sqlserver.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln("Connect DB failed:", err)
+		return
+	}
+	fmt.Println("Currently connected to DB")
+
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+
+	v1 := r.Group("/v1")
+	{
+		items := v1.Group("/items")
+		{
+			items.GET("/", getItems(db))
+			items.GET("/:id", getItem(db))
+			items.POST("/", createItem(db))
+			items.PUT("/:id")
+			items.DELETE("/")
+		}
+	}
+
+	r.Run("localhost:3000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func createItem(db *gorm.DB) func(*gin.Context) {
+	return func(c *gin.Context) {
+		var data TodoItemCreation
+		if err := c.ShouldBind(&data); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+
+		jsonData, err := json.Marshal(data)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+		fmt.Println(string(jsonData))
+
+		if err := db.Create(&data).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+
 		c.JSON(http.StatusOK, gin.H{
-			"message": item,
+			"data": data.Id,
 		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	}
+}
+
+func getItems(db *gorm.DB) func(*gin.Context) {
+	return func(c *gin.Context) {
+		var data []TodoItem
+
+		if err := db.Find(&data).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"data": data,
+		})
+	}
+}
+
+func getItem(db *gorm.DB) func(*gin.Context) {
+	return func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+
+		var data TodoItem
+
+		if err := db.Where("id = ?", id).First(&data).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"data": data,
+		})
+	}
 }
